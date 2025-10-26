7b3885c1d8f735748512b922c3ac14792e68c439
Loran Mutafov <loranmutafov@gmail.com>
2025-10-26 12:27:21 +0000

    Fix duplicate extraHosts

--- modules/kube-node.nix
+++ modules/kube-node.nix
@@ -6,19 +6,16 @@ let
   kubeMasterAPIServerPort = 6443;
 in
 {
-    networking.extraHosts = ''
-        100.111.220.64  vmi384815.contaboserver.net vmi384815
-        100.123.46.40   vmi389591.contaboserver.net vmi389591
-        100.93.103.69   vmi428314.contaboserver.net vmi428314
-        100.87.42.69    vmi430563.contaboserver.net vmi430563
-        100.65.102.102  vmi431810.contaboserver.net vmi431810
-      '';
+  networking.extraHosts = ''
+    100.111.220.64  vmi384815.contaboserver.net vmi384815
+    100.123.46.40   vmi389591.contaboserver.net vmi389591
+    100.93.103.69   vmi428314.contaboserver.net vmi428314
+    100.87.42.69    vmi430563.contaboserver.net vmi430563
+    100.65.102.102  vmi431810.contaboserver.net vmi431810
+  '';
 
   # disable swap
   swapDevices = lib.mkForce [ ];
-
-  # resolve master hostname
-  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
 
   # packages for administration tasks
   environment.systemPackages = with pkgs; [
