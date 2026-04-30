38dceba45bab74420b7f7c0eeb78826e8679f923
Loran Mutafov <loranmutafov@gmail.com>
2026-04-30 19:05:58 +0100

    Add metal-100yan to k8s cluster

--- machines/metal-100yan/definition.nix
+++ machines/metal-100yan/definition.nix
@@ -1,12 +1,17 @@
 { config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
-    # ../../modules/kube-worker/kube-worker.nix
+    ../../modules/kube-worker/kube-worker.nix
   ];
 
   time.timeZone = "Europe/Sofia";
 
   reaVaultNode = {
     enabled = true;
+  };
+
+  kubeWorkerNode = {
+    enabled = true;
+    kubeletNodeIP = "100.82.50.117";
   };
 }
